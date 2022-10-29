357565337bb0e4cc017de64d13042893d87962da
Priyanshu <priyanshukundu17120@gmail.com>
2022-10-29 21:14:11 +0530

    version 1.9.2

--- public/navbar-code.js
+++ public/navbar-code.js
@@ -1,28 +1,28 @@
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var navbar = rawFile.responseText;
-                document.write(`
+async function readTextFile(file) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        var navbar = rawFile.responseText;
+        document.write(`
 ${navbar}
 `)
-            }
-        }
+      }
     }
-    rawFile.send(null);
+  }
+  rawFile.send(null);
 }
-if(localStorage.getItem('positionofnavbarisleft')){
+
+
+
+
+if (localStorage.getItem('positionofnavbarisleft')) {
   readTextFile('/left-navbar.html')
-  }
-  else if(localStorage.getItem('positionofnavbarisfulls')){
-    readTextFile('/full-s-navbar.html')
-  }
-  else{
-    readTextFile('/top-navbar.html')
-  }
+}
+else if (localStorage.getItem('positionofnavbarisfulls')) {
+  readTextFile('/full-s-navbar.html')
+}
+else {
+  readTextFile('/top-navbar.html')
+}
